df193f0701f79378a0330b4b6cc5fff0ff6c89d0
Nathan Ho <nathan.waikin.ho@gmail.com>
2021-10-01 15:46:00 -0700

    Change to use FFT-based analyzer

--- analyze.py
+++ analyze.py
@@ -14,22 +14,29 @@ if __name__ == "__main__":
     signal, sample_rate = soundfile.read("four_notches.wav")
     signal = signal[:, 0]
 
-    frame_size = 1024
+    frame_size = int(sample_rate / start_frequency)
     hop_size = frame_size // 4
 
     frequency_signal = []
     frequency = 0
 
-    last_sample = 0
-    time_since_last_zero_crossing = 0
-    for i in range(len(signal)):
-        if last_sample <= 0 and signal[i] > 0:
-            if time_since_last_zero_crossing != 0:
-                candidate_frequency = sample_rate / time_since_last_zero_crossing
-                if start_frequency <= candidate_frequency <= end_frequency:
-                    frequency = candidate_frequency
-            time_since_last_zero_crossing = 0
-        time_since_last_zero_crossing += 1
+    warnings.simplefilter("ignore", np.RankWarning)
+
+    for offset in range(0, len(signal) - frame_size, hop_size):
+        frame = signal[offset : offset + frame_size] * scipy.signal.windows.hann(frame_size, False)
+        magnitude_spectrum = np.abs(np.fft.rfft(frame))
+        x_2 = np.argmax(magnitude_spectrum)
+        x_1 = max(x_2 - 1, 0)
+        x_3 = min(x_2 + 1, len(magnitude_spectrum) - 1)
+
+        y_1 = magnitude_spectrum[x_1]
+        y_2 = magnitude_spectrum[x_2]
+        y_3 = magnitude_spectrum[x_3]
+
+        quadratic_coefficients = np.polyfit([x_1, x_2, x_3], [y_1, y_2, y_3], deg=2)
+        bin_ = -quadratic_coefficients[1] / (2 * quadratic_coefficients[0])
+        frequency = bin_ * (sample_rate / 2) / len(magnitude_spectrum)
+
         frequency_signal.append(frequency)
 
     plt.plot(frequency_signal)
