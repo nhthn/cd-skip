696eef00473f1df2456ac2cdca0029099f9efb40
Nathan Ho <nathan.waikin.ho@gmail.com>
2021-10-05 14:05:32 -0700

    Add silence before sine sweep

--- research/generate_sine_sweep.py
+++ research/generate_sine_sweep.py
@@ -6,7 +6,9 @@ if __name__ == "__main__":
     start_frequency = 30
     end_frequency = 8000
     duration = 60 * 5
+    silence_duration = 60 * 2
 
+    num_zeros = int(sample_rate * silence_duration)
     num_samples = int(sample_rate * duration)
     signal = np.zeros(num_samples)
 
@@ -17,7 +19,10 @@ if __name__ == "__main__":
         phase = phase % 1
         signal[i] = np.sin(phase * 2 * np.pi)
 
+    signal = np.concatenate([np.zeros(num_samples), signal])
+
     soundfile.write(
+        f"silence_{silence_duration}s_"
         f"sine_sweep_{start_frequency}hz_{end_frequency}hz_{duration}s.wav",
         signal,
         sample_rate,
